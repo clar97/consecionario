27bef571860eed56cef347f79f0589e90bb40c0b
welbek01 <waramon@misena.edu.co>
2017-10-13 17:48:37 -0500

    areglo

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
-var gulp = require('gulp');
-var server = require('gulp-server-livereload');
- 
+var gulp = require('gulp'),
+server = require('gulp-server-livereload'),
+useref = require('gulp-useref'),
+gulpif = require('gulp-if'),
+uglify = require('gulp-uglify'),
+minifyCss = require('gulp-clean-css');
+
 gulp.task('server', function() {
   gulp.src('./')
     .pipe(server({
@@ -9,3 +13,20 @@ gulp.task('server', function() {
       open: true
     }));
 });
+
+gulp.task('build-html', function () {
+   return gulp.src(['*.html', 'pa*/*.html','cs*/**','j*/**'])
+       .pipe(useref())
+      .pipe(gulpif('*.js', uglify()))
+       .pipe(gulpif('*.css', minifyCss()))
+       .pipe(gulp.dest('build'));
+   });
+
+  
+gulp.task('build-assets', function () {
+  return gulp.src('image*/**').
+    pipe(gulp.dest('build'))
+  })
+
+  
+gulp.task('build', ['build-html','build-assets'])
